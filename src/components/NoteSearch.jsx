e00213d7399b44d3f8eb9e7aa4852ce8513cc267
rezafikkri <fikkri.reza@gmail.com>
2023-10-13 13:27:30 +0700

    refactor: remove button search

--- src/components/NoteSearch.jsx
+++ src/components/NoteSearch.jsx
@@ -10,9 +10,6 @@ export default class NoteSearch extends React.Component {
       <form>
         <div className="input-group">
           <input type="text" className="form-control" placeholder="Cari catatan..." aria-label="Example text with button addon" aria-describedby="button-addon1" />
-          <button className="btn btn-secondary" type="button" id="button-addon1">
-            <i className="bi bi-search"></i>
-          </button>
         </div>
       </form>
     );
