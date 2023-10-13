840388483dc2c86caecfadb4678c24a032402db3
rezafikkri <fikkri.reza@gmail.com>
2023-10-13 15:26:03 +0700

    faet: form add note functionality (like character limiter for title)

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,22 +1,69 @@
 import React from "react";
 
 export default class NoteForm extends React.Component {
+  // title character limit
+  titleCharacterLimit = 50;
+
   constructor(props) {
     super(props);
+
+    this.state = {
+      title: '',
+      body: ''
+    };
+  
+    // bind handle method
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleBodyChange = this.handleBodyChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleTitleChange(e) {
+    let title = e.target.value;
+    if (title.length > this.titleCharacterLimit) {
+      title = title.substring(0, 50);
+    }
+
+    this.setState({ title });
+  }
+
+  handleBodyChange(e) {
+    this.setState({
+      body: e.target.value
+    });
+  }
+
+  handleSubmit() {
   }
 
   render() {
     return (
-      <form>
-        <div className="form-text mb-1 text-end">Sisa karakter: 50</div>
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-text mb-1 text-end">Sisa karakter: {this.titleCharacterLimit - this.state.title.length}</div>
         <div className="form-floating mb-2">
-          <input type="email" className="form-control" id="floatingInput" placeholder="Judul catatan" />
-          <label htmlFor="floatingInput">Judul Catatan</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="Judul catatan"
+            value={this.state.title}
+            onChange={this.handleTitleChange}
+          />
+          <label htmlFor="title">Judul</label>
         </div>
+
         <div className="form-floating mb-3">
-          <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height: '100px'}}></textarea>
-          <label htmlFor="floatingTextarea2">Comments</label>
+          <textarea
+            className="form-control"
+            placeholder="Leave a comment here"
+            id="note"
+            style={{height: '100px'}}
+            onChange={this.handleBodyChange}
+            defaultValue={this.state.body}
+          ></textarea>
+          <label htmlFor="note">Isi</label>
         </div>
+
         <button type="button" className="btn btn-primary w-100 text-center">Buat</button>
       </form>
     );
